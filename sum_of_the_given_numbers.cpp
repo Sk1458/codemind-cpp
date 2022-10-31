0274b45d5dfbbb9c8714f3422d3ee45a87625667
Saikumar Voddepally <106002648+Sk1458@users.noreply.github.com>
2022-10-31 14:07:45 +0530

    #include<iostream> using namespace std;
    int main(){
        int t,num1,num2;
        cin>>t;
        while(t>0){
            cin>>num1>>num2;
            cout<<num1+num2<<endl;
            t--;
        }
        return 0;
    }

---
+++ sum_of_the_given_numbers.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int t,num1,num2;
+    cin>>t;
+    while(t>0){
+        cin>>num1>>num2;
+        cout<<num1+num2<<endl;
+        t--;
+    }
+    return 0;
+}
