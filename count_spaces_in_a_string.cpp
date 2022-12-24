1845715d7dce15452e78d1013e2c84640a0abf73
Saikumar Voddepally <106002648+Sk1458@users.noreply.github.com>
2022-12-24 23:48:58 +0530

    #include<bits/stdc++.h> using namespace std;
    int main(){
        string str;
        getline(cin,str);
        int i,c=0;
        for(i=0; i<str.length(); i++){
            if(str[i]==' '){
                c++;
            }
        }
        cout<<c;
        return 0;
    }

---
+++ count_spaces_in_a_string.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string str;
+    getline(cin,str);
+    int i,c=0;
+    for(i=0; i<str.length(); i++){
+        if(str[i]==' '){
+            c++;
+        }
+    }
+    cout<<c;
+    return 0;
+}
